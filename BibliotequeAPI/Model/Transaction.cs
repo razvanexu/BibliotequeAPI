74acc4aa1afe4ba754685574204eeb9079b091cd
Razvan Dumitrescu <razvandf@gmail.com>
2020-07-30 11:14:37 +0300

    Added props in Transaction Model

--- BibliotequeAPI/Model/Transaction.cs
+++ BibliotequeAPI/Model/Transaction.cs
@@ -7,5 +7,8 @@ namespace BibliotequeAPI.Model
 {
     public class Transaction
     {
+        public int TransactionId { get; set; }
+        public int BookId { get; set; }
+        public DateTime BorrowTime { get; set; }
     }
 }
