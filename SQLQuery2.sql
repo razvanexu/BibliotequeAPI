3eeed2a815b6974271927640ced03f6eed65965e
razvanexu <razvandf@gmail.com>
2020-08-04 23:50:42 +0300

    Repositoies

---
+++ SQLQuery2.sql
@@ -0,0 +1,8 @@
+INSERT INTO Users (FirstName, LastName, Password, Type)
+	VALUES('Razvan', 'Dumitrescu', '1234', 'Administrator');
+
+SELECT * FROM Users;
+
+INSERT INTO Users (FirstName, LastName, Password, Type)
+	VALUES('Radu', 'Escu', '1234', 'Client');
+
