a536d73ea3b3209f7c1298fd7bf2b0690532eec6
Razvan Dumitrescu <razvandf@gmail.com>
2020-08-04 18:02:19 +0300

    prepared project for ef migrations

---
+++ BibliotequeAPI/Data/Context/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using BibliotequeAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotequeAPI.Data.Context
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
